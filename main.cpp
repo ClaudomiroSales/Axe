38c9bcf805772b56e6e87ee91bf1554a17fe815f
ClaudomiroSales <claudomiro.sales@gmail.com>
2023-10-02 14:42:47 -0300

    Testando arrays e functions

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ using std::string;
 #include "Axe.h"
 #include "Axe.cpp"
 
+void printArrays( const Axe & );
+
 int main( )
 {
     cout << "Depurar Axe\n";  
@@ -31,6 +33,31 @@ int main( )
 
     ax.printAxeNames( );
 
+    Axe arr[2];
+
+    arr[ 0 ] = ax;
+    arr[ 1 ] = ax;
+
+    cout << "\n\n\n";
+    cout << "Usando arrays.\n";
+
+    arr[ 0 ].printAxeNames( );
+    arr[ 1 ].printAxeNames( );
+
+    cout << "\n\n\nUsando functions para imprimir arrays.\n";
+    printArrays( ax );
+
     return 0;
 
+}
+
+void printArrays( const Axe &ax )
+{
+    Axe arr[2];
+
+    arr[ 0 ] = ax;
+    arr[ 1 ] = ax;
+
+    arr[ 0 ].printAxeNames( );
+    arr[ 1 ].printAxeNames( );
 }
